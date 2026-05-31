STJr/SRB2

Redwall sky gone
More redwall sky shenanigans gone.

src/hardware/hw_trick.c
@@ -888,7 +888,7 @@ void HWR_CorrectSWTricks(void)
 					if (sider->bottomtexture == 0)
 					{
 						if (sider->midtexture == 0)
-							sider->bottomtexture = R_TextureNumForName("REDWALL"); // Tails
+							sider->bottomtexture = 0; // Tails // More redwall sky shenanigans
 						else
 							sider->bottomtexture = sider->midtexture;
 					}
@@ -899,7 +899,7 @@ void HWR_CorrectSWTricks(void)
 					if (sidel->bottomtexture == 0)
 					{
 						if (sidel->midtexture == 0)
-							sidel->bottomtexture = R_TextureNumForName("REDWALL"); // Tails
+							sidel->bottomtexture = 0; // Tails // More redwall sky shenanigans
 						else
 							sidel->bottomtexture = sidel->midtexture;
 					}

srb2-preservation/srb2-retro

src/hardware/hw_trick.c
@@ -898,7 +898,7 @@ void HWR_CorrectSWTricks(void)
 					if (sdr->bottomtexture == 0)
 					{
 						if (sdr->midtexture == 0)
-							sdr->bottomtexture = R_TextureNumForName("REDWALL", (UINT16)(sdr-sides)); // Tails
+							sdr->bottomtexture = 0; // Tails // More redwall sky shenanigans
 						else
 							sdr->bottomtexture = sdr->midtexture;
 					}
@@ -909,7 +909,7 @@ void HWR_CorrectSWTricks(void)
 					if (sdl->bottomtexture == 0)
 					{
 						if (sdl->midtexture == 0)
-							sdl->bottomtexture = R_TextureNumForName("REDWALL", (UINT16)(sdl-sides)); // Tails
+							sdl->bottomtexture = 0; // Tails // More redwall sky shenanigans
 						else
 							sdl->bottomtexture = sdl->midtexture;
 					}