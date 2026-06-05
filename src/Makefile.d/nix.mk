8e622ace5aa9c81dcd4df408122ea0a97eb6c2a7
PAS <pasrc@proton.me>
2026-06-04 22:52:19 -0400

    2021 Makefile backport from legacy

---
+++ src/Makefile.d/nix.mk
@@ -0,0 +1,39 @@
+#
+# Makefile options for unices (linux, bsd...)
+#
+
+ifeq (${DEDICATED},1)
+EXENAME?=lsrb2dretro
+else
+EXENAME?=lsdl2srb2retro
+endif
+
+opts+=-DUNIXCOMMON -DLUA_USE_POSIX
+libs+=-lm
+
+ifndef NOHW
+opts+=-I/usr/X11R6/include
+libs+=-L/usr/X11R6/lib
+endif
+
+SDL=1
+
+# In common usage.
+ifdef LINUX
+libs+=-lrt
+passthru_opts+=NOTERMIOS
+endif
+
+# Tested by Steel, as of release 2.2.8.
+ifdef FREEBSD
+opts+=-I/usr/X11R6/include -DLINUX -DFREEBSD
+libs+=-L/usr/X11R6/lib -lkvm
+endif
+
+ifdef OPENBSD
+libs+=-lexecinfo -lpthread
+endif
+
+ifdef MACOS
+libs+=-framework CoreFoundation
+endif
